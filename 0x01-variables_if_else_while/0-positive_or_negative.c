7060e074bf2150fe6ba80965790493367ba8eec6
majorbasit <adewunmiadeola.aa@gmail.com>
2022-09-09 13:01:09 -0700

    j

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,26 +1,50 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include <stdlib.h>                                                                                                                     
 
-/**
- * main - prints randon numbers
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int n;
+#include <time.h>                                                                                                                       
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n". n);
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
-	return (0);
-}
+#include <stdio.h>                                                                                                                      
+
+                                                                                                                                        
+
+/**                                                                                                                                     
+ *
+ *  *  main - prints random numbers                                                                                                    *
+ *   *  Return: Always 0 (success)                                                                                                          
+ *
+ *    */                                                                                                                                     
+
+int main(void)                                                                                                                          
+
+{                                                                                                                                       
+
+	        int n;                                                                                                                          
+
+		                                                                                                                                        
+
+		        srand(time(0));                                                                                                                 
+
+			        n = rand() - RAND_MAX / 2;                                                                                                      
+
+				        if (n > 0)                                                                                                                      
+
+						                printf("%d is positive\n", n);                                                                                          
+
+					        else if (n == 0)                                                                                                                
+
+							        {                                                                                                                               
+
+									                printf("%d is zero\n", n);                                                                                              
+
+											        }                                                                                                                               
+
+						        else                                                                                                                            
+
+								        {                                                                                                                               
+
+										                printf("%d is negative\n", n);                                                                                          
+
+												        }                                                                                                                               
+
+							        return (0);             
+
+								
