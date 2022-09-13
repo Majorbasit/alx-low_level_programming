6a414b07f3e069dbe06f6b54758ffa751f65af09
majorbasit <adewunmiadeola.aa@gmail.com>
2022-09-12 17:22:52 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,50 +1,28 @@
-#include <stdlib.h>                                                                                                                     
-
-#include <time.h>                                                                                                                       
-
-#include <stdio.h>                                                                                                                      
-
-                                                                                                                                        
-
-/**                                                                                                                                     
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
  *
- *  *  main - prints random numbers                                                                                                    *
- *   *  Return: Always 0 (success)                                                                                                          
- *
- *    */                                                                                                                                     
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
 
-int main(void)                                                                                                                          
-
-{                                                                                                                                       
-
-	        int n;                                                                                                                          
-
-		                                                                                                                                        
-
-		        srand(time(0));                                                                                                                 
-
-			        n = rand() - RAND_MAX / 2;                                                                                                      
-
-				        if (n > 0)                                                                                                                      
-
-						                printf("%d is positive\n", n);                                                                                          
-
-					        else if (n == 0)                                                                                                                
-
-							        {                                                                                                                               
-
-									                printf("%d is zero\n", n);                                                                                              
-
-											        }                                                                                                                               
-
-						        else                                                                                                                            
-
-								        {                                                                                                                               
-
-										                printf("%d is negative\n", n);                                                                                          
-
-												        }                                                                                                                               
-
-							        return (0);             
-
-								
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
